1c21a9a8ce5e4e7ada217b27a7c498f4efe081d6
TomasCacoete <tomaspcacoete@gmail.com>
2024-12-06 02:54:45 +0000

    changed router

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import LandingPage from './pages/LandingPage/LandingPage';
 import TeamPage from './pages/TeamPage/TeamPage';
 
 function App()  {
   return (
     <>
-      <Router>
+      <HashRouter>
             <Routes>
               <Route path="/tour-dash" element={<LandingPage/>}/>
               <Route path="/tour-dash/team" element={<TeamPage/>}/>
             </Routes>
-      </Router>
+      </HashRouter>
     </>
   );
 }
