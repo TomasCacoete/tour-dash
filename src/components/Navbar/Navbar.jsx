7b0a81975b6c23743917345f727debda15923ab6
TomasCacoete <tomaspcacoete@gmail.com>
2024-12-06 03:24:07 +0000

    whatever

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -5,9 +5,9 @@ import LogoWhite from "../../assets/logo-white.svg"
 export default function Navbar(){
     return(
         <nav>
-            <a href="/tour-dash/"><img className="nav-logo" src={LogoWhite}/></a>
-            <a href="/tour-dash/team" className="nav-element">Team</a>
-            <a href="/tour-dash/#contacts" className="nav-element">Contacts</a>
+            <a href="/tour-dash/#/tour-dash/"><img className="nav-logo" src={LogoWhite}/></a>
+            <a href="/tour-dash/#/tour-dash/team/" className="nav-element">Team</a>
+            <a className="nav-element">Contacts</a>
         </nav>
     )
 }
